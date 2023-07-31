1bb36e9b5954cfa65e9e7716716b7fb690ba3a19
Sebastien Despres <seb.despres@gmail.com>
2023-07-31 16:31:27 -0700

    updated db with retailer info

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -2,7 +2,7 @@ import { json } from '@sveltejs/kit'
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch, params }) {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+    const res = await fetch(`https://my-json-server.typicode.com/vortexEditor/retailer/posts`);
     const retailerData = await res.json();
 
     return { retailerData };
